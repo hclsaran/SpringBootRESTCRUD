b12e4862dcbcaa6e112e249653b7634436419565
hclsaran <77113470+hclsaran@users.noreply.github.com>
2022-02-23 17:43:59 -0500

    Update SpringBootRestcrudApplicationTests.java

--- src/test/java/com/hcl/SpringBootRestcrudApplicationTests.java
+++ src/test/java/com/hcl/SpringBootRestcrudApplicationTests.java
@@ -1,13 +1,1 @@
-package com.hcl;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SpringBootRestcrudApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
